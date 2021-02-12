9049d4d94d2ac591e47e4aa308be7c258f887fd9
Axel Gromat <axel.gromat@gmail.com>
2021-02-12 12:13:30 +0100

    Finish

--- src/App.vue
+++ src/App.vue
@@ -11,27 +11,55 @@
       <span v-if="clicks>10">Calme-toi stp</span>
       <div v-if="clicks>100">Mec respire wtf</div>
     </div>
+    <Logo :bienvenue="message"/>
     <p>Vue permet de modifier des valeurs en considérant l'interaction utilisateur avec v-on ou @</p>
     <button @click="clicked" id="clicks">
       Clicks: {{ clicks }}
     </button>
+    <Logo/>
     <p>Vue permet aussi le 2-way binding, c'est des valeurs affichées en temps réel, qui sont aussi modifiables avec v-model</p>
     <h2>{{ message }}</h2>
     <input v-model="message"/>
+    <Logo/>
+    <ul id="array-rendering">
+      <li v-for="item in items" :key="item.name">
+        {{ item.name }} only for {{ item.price }}
+      </li>
+    </ul>
+    <p>Vue permet de créer des composants exportables qui permettent de créer des templates prédéfinis et réutilisables "à l'infini"</p>
+    <Logo/>
+    <p>Ces composants peuvent comporter des props, c'est à dire des éléments qui varieront en fonction des données qui leur sont attribuées.</p>
+    <Logo/>
+    <p>Ces props sont assignables à des éléments statiques mais aussi à des variables/éléments dynamiques qui s'afficheront si ils sont définis et qui utiliseront la procédure de fallback définies au préalable (dans le cas où aucune fallback procedure n'est définie, l'élément ne s'affichera tout simplement pas)</p>
+    <Logo :bienvenue="message"/>
+    <p>Pour effectuer des transferts de données depuis l'enfant vers le parent, l'enfant doit initialiser l'action et le parent en récupérant cette action effectuera la modification de la donnée</p>
+    <Logo bienvenue="Un message différent" v-on:sendBounce="squareIt"
+    :bounces="bounces"/>
   </div>
 </template>
 
 <script>
+import Logo from './components/Logo'
 
 export default {
   name: 'App',
   components: {
+    Logo,
   },
   data(){
     return {
       counter:0,
       clicks:0,
       message: "Salut !",
+      items : [
+        { name : 'TV LG15ED56 4K',price:"899€"},
+        { name : 'Sony PlayStation 5',price:"599€"},
+        { name : 'Xbox Series X',price:"499€"},
+        { name : 'Apple iPhone 12',price:"999€"},
+        { name : 'Samsung Galaxy S21',price:"899€"},
+        { name : 'TV Samsung 1H 4K',price:"999€"},
+      ],
+      bounces:2,
     }
   },
   mounted() {
@@ -42,6 +70,10 @@ export default {
   methods : {
     clicked(){
       this.clicks++;
+    },
+    squareIt(){
+      this.bounces = this.bounces*this.bounces;
+      return this.bounces;
     }
   }
 }
---
+++ src/components/Logo.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <img src="../assets/logo.png" alt="Vue logo">
+        <p>{{ bienvenue || "Bienvenue !"}}  sur mon site</p>
+        <button @click="sendBounce"> Bounces with parent  : {{ bounces }} </button>
+    </div>
+</template>
+<script>
+export default {
+    name:"Logo",
+    props:["bienvenue","bounces"],
+    data(){
+        return {
+            real:true,
+        }
+    },
+    methods:{
+        sendBounce(){
+            this.$emit('sendBounce');
+        }
+    }
+}
+</script>
+<style scoped>
+    img{
+        height:200px;
+    }
+</style>
